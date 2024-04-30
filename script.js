a393dd340d3fa46dc259ca890d4e17dbf4bd66fe
Emanuel Gifuni <emanuelgifuni@yahoo.ie>
2024-04-30 12:17:35 +0200

    JS code

---
+++ script.js
@@ -0,0 +1,14 @@
+window.scroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if(window.scrollY >= sticky) {
+        navbar.classList.add("sticky")
+    }
+    else {
+        navbar.classList.remove("sticky");
+    }
+}
